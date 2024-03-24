1fbd67e5b814fce6aed46f481fca2bca71ef93ce
Pothisam <pothiinfo@gmail.com>
2024-03-24 11:47:30 +0530

    updated to login

--- src/app/Shared/MatHelper/MatHelper/MatHelper.module.ts
+++ src/app/Shared/MatHelper/MatHelper/MatHelper.module.ts
@@ -1,27 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatHelperComponent } from './MatHelper.component';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { TextboxComponent } from '../textbox/textbox.component';
 import { FormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { PasswordComponent } from '../password/password.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ButtonComponent } from '../button/button.component';
 import { SelectComponent } from '../select/select.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatListModule} from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
 import { AutocompleteComponent } from '../autocomplete/autocomplete.component';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { TableComponent } from '../table/table.component';
-import {  MatTableModule } from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
-import {MatSortModule} from '@angular/material/sort';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CardComponent } from '../card/card.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -39,9 +40,32 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
     MatTableModule,
     MatSort,
     MatSortModule,
-    MatCheckboxModule
+    MatCheckboxModule,
   ],
-  declarations: [MatHelperComponent,TextboxComponent,PasswordComponent,ButtonComponent,SelectComponent,AutocompleteComponent,TableComponent],
-  exports:[MatProgressSpinnerModule,MatGridListModule,MatCardModule,MatInputModule,TextboxComponent,MatIconModule,FormsModule,PasswordComponent,ButtonComponent,SelectComponent,AutocompleteComponent,TableComponent]
+  declarations: [
+    MatHelperComponent,
+    TextboxComponent,
+    PasswordComponent,
+    ButtonComponent,
+    SelectComponent,
+    AutocompleteComponent,
+    TableComponent,
+    CardComponent,
+  ],
+  exports: [
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatCardModule,
+    MatInputModule,
+    TextboxComponent,
+    MatIconModule,
+    FormsModule,
+    PasswordComponent,
+    ButtonComponent,
+    SelectComponent,
+    AutocompleteComponent,
+    TableComponent,
+    CardComponent,
+  ],
 })
-export class MatHelperModule { }
+export class MatHelperModule {}
